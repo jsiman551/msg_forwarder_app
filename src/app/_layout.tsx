9d37d8b8c7e06aa795c251f78a49c2979c49bc7b
Jose Simancas <josesimancas51@gmail.com>
2026-09-01 11:56:06 -0500

    add NativeWind v5 to project

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,23 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useColorScheme } from 'react-native';
+import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from "expo-router";
+import { useColorScheme } from "react-native";
+import "../global.css";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-SplashScreen.preventAutoHideAsync();
-
-export default function TabLayout() {
+export default function Layout() {
   const colorScheme = useColorScheme();
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: colorScheme === "dark" ? "#000" : "#fff",
+          },
+          headerTintColor: colorScheme === "dark" ? "#fff" : "#000",
+          contentStyle: {
+            backgroundColor: colorScheme === "dark" ? "#111" : "#fafafa",
+          },
+        }}
+      />
     </ThemeProvider>
   );
 }
